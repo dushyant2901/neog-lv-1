1c685c374284b0287b72357a0c5c7fcaef3aa245
Dushyant Pratap Singh <singhdushyant2901@gmail.com>
2023-02-23 17:57:50 +0530

    practice set-1 ques-11

---
+++ session-1/practice set 1/ques-11.js
@@ -0,0 +1,10 @@
+const product = {
+  title: "iPhone",
+  price: 5999,
+  description: "The iPhone is a smartphone developed by Apple",
+};
+
+const { title, price, description } = product;
+console.log(`The title is '${title}'`);
+console.log(`The price is '${price}'`);
+console.log(`The description is '${description}'`);
