357a5f3ee12c5bea52ef13a0dc13e8c40b3a506b
Dushyant Pratap Singh <singhdushyant2901@gmail.com>
2023-02-23 17:29:31 +0530

    practice set-1 ques-4

---
+++ session-1/practice set 1/ques-4.js
@@ -0,0 +1,8 @@
+const isEvenOrOdd = (num) =>
+  num % 2 === 0 ? "The given number is even" : `The given number is odd`;
+
+console.log(isEvenOrOdd(12));
+console.log(isEvenOrOdd(122));
+console.log(isEvenOrOdd(121));
+console.log(isEvenOrOdd(1));
+console.log(isEvenOrOdd(1342));
