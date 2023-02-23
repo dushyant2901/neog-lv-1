c7cc4107bd56c763e5eac3b3e3b5b8489c1b6694
Dushyant Pratap Singh <singhdushyant2901@gmail.com>
2023-02-23 17:25:55 +0530

    practice set-1 ques-3

---
+++ session-1/practice set 1/ques-3.js
@@ -0,0 +1,6 @@
+const checkNum = (num) => (num >= 0 ? `positive` : `negative`);
+console.log(checkNum(12));
+console.log(checkNum(0));
+console.log(checkNum(-12));
+console.log(checkNum(-2));
+console.log(checkNum(182));
