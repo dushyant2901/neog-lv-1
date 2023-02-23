924cb7065c6beb559412aa780ab7bb022c629683
Dushyant Pratap Singh <singhdushyant2901@gmail.com>
2023-02-23 17:22:11 +0530

    practice set-1 ques-2

---
+++ session-1/practice set 1/ques-2.js
@@ -0,0 +1,10 @@
+const whichNumIsGreater = (num1, num2) =>
+  num1 > num2
+    ? `Number-1 is greater than Number-2`
+    : `Number-2 is greater than Number-2`;
+
+console.log(whichNumIsGreater(132, 34));
+console.log(whichNumIsGreater(12, 34));
+console.log(whichNumIsGreater(132, 334));
+console.log(whichNumIsGreater(1322, 34));
+console.log(whichNumIsGreater(132, 2334));
