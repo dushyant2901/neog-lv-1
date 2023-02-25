525ca751e91eca23d6badf356ac86f4686c87d61
Dushyant Pratap Singh <singhdushyant2901@gmail.com>
2023-02-25 18:33:01 +0530

    practice set-3 ques-14

---
+++ session-1/practice set-3/ques-14.js
@@ -0,0 +1,15 @@
+const printData = ({ name, country }) => `${name} lives in ${country}`;
+
+const userData1 = {
+  name: "Gaurav",
+  postalCode: "12134",
+  country: "Japan",
+};
+console.log(printData(userData1)); // Gaurav lives in Japan
+
+const userData2 = {
+  name: "Pritam",
+  postalCode: "560223",
+  country: "India",
+};
+console.log(printData(userData2)); // Pritam lives in India
