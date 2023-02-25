d6e783619dad574c48a36ba55c8546c15906ad9b
Dushyant Pratap Singh <singhdushyant2901@gmail.com>
2023-02-25 18:37:11 +0530

    practice set-3 ques-15

---
+++ session-1/practice set-3/ques-15.js
@@ -0,0 +1,12 @@
+const printProductDetails = ({ name, specification: { capacity, size } }) =>
+  `${name} which is of ${size} inch, has a capacity of ${capacity}.`;
+
+const product = {
+  name: "Apple MacBook Air 2020",
+  specification: {
+    capacity: "256GB",
+    size: "13.3 Inch",
+  },
+  price: 82900,
+};
+console.log(printProductDetails(product)); // Apple MacBook Air 2020 which is of 13.3 Inch, has a capacity of 256GB.
