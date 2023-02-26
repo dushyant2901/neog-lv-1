ba360298e42d5a3537bffcc0d9e04c24aa6be5a5
Dushyant Pratap Singh <singhdushyant2901@gmail.com>
2023-02-26 20:16:04 +0530

    practice set-4 ques-8

---
+++ session-1/practice set-4/ques-8.js
@@ -0,0 +1,13 @@
+const checkForTeam = (user) => {
+  if (!user.team) user.team = " A";
+  return user;
+};
+
+console.log(checkForTeam({ firstName: "Penn", lastName: "Ma" }));
+// {firstName: 'Penn', lastName: 'Ma', team: A}
+
+console.log(checkForTeam({ firstName: "John", lastName: "Dee", team: "B " }));
+// {firstName: 'John', lastName: 'Dee', team: B}
+
+console.log(checkForTeam({ firstName: "Priya", lastName: "Raj" }));
+// {firstName: 'Priya', lastName: 'Raj', team: A}
